98276cacc2a2399eb62093c1958bbf0ee4f2cb4e
KauanFelipe2301 <kauan.biazoli.neves@escola.pr.gov.br>
2024-07-24 08:18:14 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,15 @@
+//variáveis da bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 15;
+let raio = diametro / 2 ;
+  
+//velocidade da bolinha                                                                           
+let velocidadeXBolinha = 2;
+let velocidadeYBolinha = 2;
+let raqueteComprimento = 2;
+let raqueteAltura = 90;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
